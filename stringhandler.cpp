d877c95f2ebd07993ae406dafe2667638218a120
Leesoo <altnfsudwo@naver.com>
2021-10-13 13:43:21 +0900

    isdigit()
    문자면 0을 return, 숫자면 그 숫자를 return

---
+++ stringhandler.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <cctype>
+
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+    
+    if(s.size() == 4 || s.size() == 6 )
+    {
+        for(int i = 0; i<s.length(); i++)
+        {
+            if(isdigit(s[i]) == 0)
+            {
+                answer = false;
+                break;
+            }
+         }
+    }
+    else
+        answer = false;
+   
+    return answer;
+}
